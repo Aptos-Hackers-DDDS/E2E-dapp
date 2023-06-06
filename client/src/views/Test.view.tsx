75ffbebac790b7266f76bef424bae5308904b448
damos <damiennarozniak@yahoo.com>
2023-06-06 09:31:14 +0200

    remove constatnt.ts add wallet selector

--- client/src/views/Test.view.tsx
+++ client/src/views/Test.view.tsx
@@ -1,9 +1,26 @@
-import { Box, Typography } from "@mui/joy";
+import { useState, useEffect } from "react";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+
+import { Provider, Network } from "aptos";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+
+import { Box, Button, Typography } from "@mui/joy";
 
 export const TestView = () => {
+  const { account, signAndSubmitTransaction } = useWallet();
+  useEffect(() => {
+    ;
+  }, [account?.address]);
+
+
+
   return (
     <Box>
       <Typography level="h2">Test</Typography>
+
+      <Button>Test</Button>
+      <WalletSelector  />
+
     </Box>
   );
 };
