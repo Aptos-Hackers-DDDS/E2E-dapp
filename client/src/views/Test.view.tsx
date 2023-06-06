5071824828ab7349871556d38cb5443072c1ed30
damos <damiennarozniak@yahoo.com>
2023-06-06 11:47:22 +0200

    upload mat

--- client/src/views/Test.view.tsx
+++ client/src/views/Test.view.tsx
@@ -1,4 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,ChangeEvent } from "react";
+
+
+
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 
 import { Provider, Network } from "aptos";
@@ -6,13 +9,43 @@ import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
 import { Box, Button, Typography } from "@mui/joy";
 
+
+
 export const TestView = () => {
   const { account, signAndSubmitTransaction } = useWallet();
   useEffect(() => {
     ;
   }, [account?.address]);
 
+  const [fileList, setFileList] = useState<FileList | null>(null);
 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFileList(e.target.files);
+  };
+
+  const handleUploadClick = () => {
+    if (!fileList) {
+      return;
+    }
+
+    // 👇 Create new FormData object and append files
+    const data = new FormData();
+    files.forEach((file, i) => {
+      data.append(`file-${i}`, file, file.name);
+    });
+
+    // 👇 Uploading the files using the fetch API to the server
+    fetch('https://httpbin.org/post', {
+      method: 'POST',
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  };
+
+  // 👇 files is not an array, but it's iterable, spread to get an array of files
+  const files = fileList ? [...fileList] : [];
 
   return (
     <Box>
@@ -21,6 +54,21 @@ export const TestView = () => {
       <Button>Test</Button>
       <WalletSelector  />
 
+      <div>
+      <input type="file" onChange={handleFileChange} multiple />
+
+      <ul>
+        {files.map((file, i) => (
+          <li key={i}>
+            {file.name} - {file.type}
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleUploadClick}>Upload</button>
+    </div>
+
+      
     </Box>
   );
 };
